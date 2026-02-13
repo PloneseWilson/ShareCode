1c79ba5b9a69fe75a225c6cae3a8f6a462967c78
Wilson Liu WZ <148373705+PloneseWilson@users.noreply.github.com>
2026-02-13 21:45:17 +0800

    Add files via upload

---
+++ CS2107_Assignment1/script.py
@@ -0,0 +1,14 @@
+###ncat cs2107-ctfd-i.comp.nus.edu.sg 5003
+from pwn import * # Import pwntools
+
+
+ans = binascii.unhexlify("4353323130377b7768305f6e333364735f70616464316e675f7768336e5f7930755f683476335f6563627d0505050505")
+print(ans)
+
+r = remote("cs2107-ctfd-i.comp.nus.edu.sg", 5003)
+'''
+r.recvuntil(b"") 
+r.sendline(b"")             
+'''
+r.interactive()
+    
